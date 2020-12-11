b2f4b1fe05cbbe6491bebb6ea4dcae3268ebd174
student <student@learncodinganywhere.com>
2020-12-10 22:16:30 -0800

    Add project files.

---
+++ ClassObjectMethodStep231/Classy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectMethodStep231
+{
+    public class Classy
+    {
+        public void ClassyNew(out int num1)
+        {
+            Console.WriteLine("Enter a number ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + " divided by 2 is : " + num1 / 2);
+        }
+        public void  ClassyNew(out int P, out int B)
+        {
+            P = 10;
+            B = 12;
+
+        }
+
+    }
+}
---
+++ ClassObjectMethodStep231/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectMethodStep231
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Classy ClueLess = new Classy();
+            int a;
+            ClueLess.ClassyNew(out a);
+
+
+            int b, c;
+            ClueLess.ClassyNew(out b, out c );
+            Console.WriteLine("the value of input is {0}\n", b);
+            Console.WriteLine("the value of input is {0}\n", c);
+            Console.ReadLine();
+        }
+ 
+        
+    }
+}
